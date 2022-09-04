424c4018400a02b1db0cb10455d1ab9368d1d7da
MMA <lesnic1987@mail.ru>
2022-09-04 11:17:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int Fun()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число ''А'': ");
+    int А = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine("Введите число ''В'': ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    int Count = А;
+
+    for (int i = 1; i < B; i++)
+    {
+        Count = Count * А;
+    }
+    return Count;
+    }
+
+try
+{
+    Console.WriteLine($"A в степени B равно: {Fun()}");
+}
+catch
+{
+    Console.WriteLine("Необходимо вводить числа");
+}
